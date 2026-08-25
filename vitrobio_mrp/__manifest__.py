07619064ec0aa5d27dc025a61cd54e692bc0f170
lanciaux donovan <d.lanciaux@vitrobio.com>
2026-08-25 09:16:34 +0200

    init mrp module

---
+++ vitrobio_mrp/__manifest__.py
@@ -0,0 +1,17 @@
+﻿{
+    "name": "Vitrobio MRP",
+    "version": "17.0.1.0.0",
+    "category": "Vitrobio",
+    "summary": "Vitrobio MRP",
+    "author": "Vitrobio",
+    "license": "LGPL-3",
+    "depends": [
+        "base"
+    ],
+    "data": [
+        "security/ir.model.access.csv"
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False
+}
